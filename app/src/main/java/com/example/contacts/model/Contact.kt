d402b7c590b607b3194ab2ce0c8b845af863dbd0
Guil Dana <hil.dana2904@gmail.com>
2023-11-12 18:07:00 +0300

    Model is created

---
+++ app/src/main/java/com/example/contacts/model/Contact.kt
@@ -0,0 +1,10 @@
+package com.example.contacts.model
+
+import com.github.javafaker.PhoneNumber
+
+data class Contact(
+    val id: Long,
+    val firstName: String,
+    val lastName: String,
+    val phoneNumber: PhoneNumber
+)
---
+++ app/src/main/java/com/example/contacts/model/ContactsService.kt
@@ -0,0 +1,60 @@
+package com.example.contacts.model
+
+import com.github.javafaker.Faker
+import java.util.*
+
+typealias ContactListener = (contactsList: List<Contact>) -> Unit
+
+class ContactService {
+    private var contacts = mutableListOf<Contact>()
+
+    private val listeners = mutableSetOf<ContactListener>()
+
+    init {
+        val faker = Faker.instance()
+        val generatedContacts = (1..100).map {
+            Contact(
+                id = it.toLong(),
+                firstName = faker.name().firstName(),
+                lastName = faker.name().lastName(),
+                phoneNumber = faker.phoneNumber()
+            )
+        }
+    }
+
+    fun getContacts(): List<Contact>{
+        return contacts
+    }
+
+    fun addContact(contact: Contact){
+        contacts.add(contact)
+        notifyChanges()
+    }
+
+    fun deleteContact(contact: Contact){
+        contacts.remove(contact)
+        notifyChanges()
+    }
+
+    fun moveContact(contact: Contact, moveBy: Int){
+        val oldIndex = contacts.indexOfFirst { it.id == contact.id }
+        if(oldIndex == -1) return
+        val newIndex = oldIndex + moveBy
+        if(newIndex < 0 || newIndex >= contacts.size) return
+        Collections.swap(contacts, oldIndex, newIndex)
+        notifyChanges()
+    }
+
+    fun addListener(listener: ContactListener){
+        listeners.add(listener)
+        listener.invoke(contacts)
+    }
+
+    fun removeListener(listener: ContactListener){
+        listeners.remove(listener)
+    }
+
+    private fun notifyChanges(){
+        listeners.forEach{it.invoke(contacts)}
+    }
+}
