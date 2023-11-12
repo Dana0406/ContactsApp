7b73f24bd61d25cc840e239eb1d37f715ec703b4
Guil Dana <hil.dana2904@gmail.com>
2023-11-12 17:26:52 +0300

    Buttons and actionBar are edit

--- app/src/main/java/com/example/contacts/MainActivity.kt
+++ app/src/main/java/com/example/contacts/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.contacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_delete,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
