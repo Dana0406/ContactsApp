118b755f7d9d4fe3dbc3612ee9be6e24ca27a985
Guil Dana <hil.dana2904@gmail.com>
2023-11-12 19:41:44 +0300

    Adapter is added

---
+++ app/src/main/java/com/example/contacts/ContactsAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.contacts
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.contacts.databinding.ItemContactBinding
+import com.example.contacts.model.Contact
+
+class ContactsAdapter : RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder>() {
+
+    var contacts: List<Contact> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemContactBinding.inflate(inflater, parent, false)
+        return ContactsViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return contacts.size
+    }
+
+    override fun onBindViewHolder(holder: ContactsViewHolder, position: Int) {
+        val contact = contacts[position]
+        with(holder.binding){
+            surnameNameTextView.text = contact.firstName + " " + contact.lastName
+            phoneNumberTextView.text = contact.phoneNumber.toString()
+        }
+    }
+
+    class ContactsViewHolder(
+        val binding: ItemContactBinding
+    ) : RecyclerView.ViewHolder(binding.root)
+}
